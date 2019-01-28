61482df744707027df18cda575f55c498a36ad34
Matthew Wilson <wilsonmatt219@gmail.com>
2019-01-28 19:19:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ function divide(a, b){
   number = a / b;
   return number;
 }
+
 function inc(a){
   number = a + 1;
   return number;
 }
+
+function dec(a){
+  number = a - 1;
+  return number;
+}
+
+function parseInt
